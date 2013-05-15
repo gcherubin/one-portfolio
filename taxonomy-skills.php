75436da11c16feb6322cb190a6f18dde81ca2543
gcherubin <gianlucacherubin@gmail.com>
2013-05-14 20:13:26 -0400

    Third Commit

---
+++ taxonomy-skills.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package One Portfolio
+ */
+
+get_header(); ?>
+	<?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); ?>
+	<section id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title"><?php echo $term->name; ?></h1>
+				<?php
+					if ( is_category() ) :
+						// show an optional category description
+						$category_description = category_description();
+						if ( ! empty( $category_description ) ) :
+							echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
+						endif;
+
+					elseif ( is_tag() ) :
+						// show an optional tag description
+						$tag_description = tag_description();
+						if ( ! empty( $tag_description ) ) :
+							echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
+						endif;
+
+					endif;
+				?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content-portfolio', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php one_portfolio_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'no-results', 'archive' ); ?>
+
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
