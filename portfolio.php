ccdfede2aebb61e94946dee2db36405e5e6b45a9
gcherubin <gianlucacherubin@gmail.com>
2013-05-17 12:58:00 -0400

    Updated Intro Bio

--- portfolio.php
+++ portfolio.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="intro" class="site-intro">
-			<h1>UI Designer & Frontend Developer in Brooklyn, NY.</h1>
+			<h1>UI Designer &<br>Frontend Developer<br>in Brooklyn, NY.</h1>
 			<a href="#" class="scrolltoworks">Works</a>
 		</div>
 		<div id="content" class="site-content" role="main">
